ff49e3680d6d40c1653f63fbda41e987e30ac7f0
Christian Bay <taz@nakke.dk>
2012-02-27 23:13:36 +0100

    Add project: TestProgramWhileTrue

---
+++ Tests/TestProgramWhileTrue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+
+namespace TestProgramWhileTrue
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			while (true)
+			{
+				Thread.Sleep(5000);
+			}
+		}
+	}
+}
---
+++ Tests/TestProgramWhileTrue/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProgramWhileTrue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("TestProgramWhileTrue")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("49bea376-73f2-4dbe-86a2-f5c7ff6ac1fc")]
