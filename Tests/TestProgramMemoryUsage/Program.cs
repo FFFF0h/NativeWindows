576f492944eeb3474a1539c6e303c272c908b067
Christian Bay <taz@nakke.dk>
2012-02-27 23:13:12 +0100

    Add project: TestProgramMemoryUsage

---
+++ Tests/TestProgramMemoryUsage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestProgramMemoryUsage
+{
+	public class Program
+	{
+		public static int Main(string[] args)
+		{
+			var data = new byte[1000][];
+			try
+			{
+				for (int i = 0; i < 1000; i++)
+				{
+					data[i] = new byte[1024 * 1024];
+					for (int j = 0; j < data[i].Length; j++)
+					{
+						data[i][j] = (byte) (j % 255);
+					}
+					Console.WriteLine("{0} MB allocated", i);
+				}
+			}
+			catch(OutOfMemoryException)
+			{
+				Console.WriteLine("Out of memory");
+				return -1;
+			}
+			return 0;
+		}
+	}
+}
---
+++ Tests/TestProgramMemoryUsage/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProgramMemoryUsage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("TestProgramMemoryUsage")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("06351dd5-100d-4415-a435-a386703c2d0f")]
