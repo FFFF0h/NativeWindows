9c55cc2cf6b9d4cff0681dc348c5152c1c5f68da
Christian Bay <taz@nakke.dk>
2012-02-27 23:04:54 +0100

    Add class: IntPtrExtensions

---
+++ NativeWindows/IntPtrExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NativeWindows
+{
+	public static class IntPtrExtensions
+	{
+		private static class NativeMethods
+		{
+			[DllImport("kernel32.dll", SetLastError = true)]
+			public static extern bool CloseHandle(IntPtr handle);
+		}
+
+		public static bool CloseHandle(this IntPtr handle)
+		{
+			if (handle == IntPtr.Zero)
+			{
+				return true;
+			}
+			return NativeMethods.CloseHandle(handle);
+		}
+	}
+}
