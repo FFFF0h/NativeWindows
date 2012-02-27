7a3466e78899d746da7f3f5771d37ca9a3d02d30
Christian Bay <taz@nakke.dk>
2012-02-27 23:14:02 +0100

    Add project: JobObjectDemoProgram

---
+++ Demo/JobObjectDemoProgram/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using NativeWindows.JobObject;
+
+namespace JobObjectDemoProgram
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			string filepath = Path.Combine(typeof(TestProgramMemoryUsage.Program).Assembly.Location);
+			var process1 = Process.Start(filepath);
+			var process2 = Process.Start(filepath);
+
+			var security = new JobObjectSecurity(true);
+			security.AddAccessRule(new NTAccount("Everyone"), JobObjectAccessRights.All, AccessControlType.Allow);
+
+			using (var job = JobObjectHandle.Create(security, "Foobar"))
+			{
+				var extendedLimitInformation = new ExtendedLimitInformation
+				{
+					KillOnJobClose = true,
+					JobMemoryLimit = 3 * 1024 * 1024 * 1024UL,
+					ProcessMemoryLimit = 100 * 1024 * 1024,
+				};
+				job.SetInformation(extendedLimitInformation);
+
+				job.AssignProcess(process1);
+
+				using (var job2 = JobObjectHandle.Open("Foobar"))
+				{
+					job2.AssignProcess(process2);
+					var extendedLimitInformation2 = job2.GetInformation<ExtendedLimitInformation>();
+					var basicAccountingInformation = job2.GetInformation<BasicAndIoAccountingInformation>();
+					var basicUiRestrictions = job2.GetInformation<BasicUiRestrictions>();
+				}
+
+				Console.WriteLine("Press any key to exit...");
+				Console.ReadKey(intercept: true);
+			}
+		}
+	}
+}
---
+++ Demo/JobObjectDemoProgram/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JobObjectDemoProgram")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("JobObjectDemoProgram")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("f03f6a10-4562-4bca-83ab-9207ffaaaae3")]
