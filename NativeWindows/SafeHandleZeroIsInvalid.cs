826d0c4f029d1c902a878c28bfba7ffce2dea47d
Christian Bay <taz@nakke.dk>
2012-02-27 23:06:53 +0100

    Add class: SafeHandleZeroIsInvalid (why on earth does not exists pr default?)

---
+++ NativeWindows/SafeHandleZeroIsInvalid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+
+namespace NativeWindows
+{
+	[SecurityCritical]
+	[SecurityPermission(SecurityAction.InheritanceDemand, UnmanagedCode = true)]
+	public abstract class SafeHandleZeroIsInvalid : SafeHandle
+	{
+		[ReliabilityContract(Consistency.WillNotCorruptState, Cer.MayFail)]
+		protected SafeHandleZeroIsInvalid(bool ownsHandle)
+			: base(IntPtr.Zero, ownsHandle)
+		{
+		}
+
+		public override bool IsInvalid
+		{
+			[SecurityCritical]
+			get
+			{
+				return handle == IntPtr.Zero;
+			}
+		}
+	}
+}
