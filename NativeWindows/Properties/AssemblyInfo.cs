a036b292bb2730621ff5bdc3f8b45f47058d2a9b
Christian Bay <taz@nakke.dk>
2012-02-27 23:03:48 +0100

    Added: NativeWindows.sln and .csproj

---
+++ NativeWindows/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NativeWindows")]
+[assembly: AssemblyDescription("Easy to use wrapper for native windows functionality")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("NativeWindows")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("ec4f14d6-ec41-4681-b2dc-b24ec17b9b41")]
