cb79ff1418bded9933bb90d3a3a1b7ae2cab2883
Christian Bay <taz@nakke.dk>
2012-02-27 22:59:40 +0100

    Added: Global assembly info

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("AppHarbor")]
+[assembly: AssemblyCopyright("Copyright © AppHarbor 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
