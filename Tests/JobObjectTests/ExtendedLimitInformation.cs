d81f88795db7b2f70857e16b6e5448679cb7b34a
Christian Bay <taz@nakke.dk>
2012-02-27 23:15:11 +0100

    Add project: JobObjectIntegrationTests

---
+++ Tests/JobObjectTests/ExtendedLimitInformation.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using NativeWindows.JobObject;
+using Xunit;
+
+namespace JobObjectTests
+{
+	public class ExtendedLimitInformationTest
+	{
+		[Fact]
+		public void GivenJobWithKillOnJobClose_WhenDisposingJob_ThenProcessIsStopped()
+		{
+			Process process = Process.Start(typeof(TestProgramWhileTrue.Program).Assembly.Location);
+
+			using (var job = JobObjectHandle.Create())
+			{
+				job.SetInformation(new ExtendedLimitInformation
+				{
+					KillOnJobClose = true,
+				});
+				job.AssignProcess(process);
+			}
+
+			Assert.True(process.HasExited);
+			Assert.Equal(0, process.ExitCode);
+		}
+
+		[Fact]
+		public void GivenJobObjectWithoutMemoryLimit_ThenOutOfMemoryIsNotThrown()
+		{
+			Process process = Process.Start(typeof(TestProgramMemoryUsage.Program).Assembly.Location);
+			using (var job = JobObjectHandle.Create())
+			{
+				job.SetInformation(new ExtendedLimitInformation
+				{
+				});
+				job.AssignProcess(process);
+			}
+
+			process.WaitForExit();
+
+			Assert.Equal(0, process.ExitCode);
+		}
+
+		[Fact]
+		public void GivenJobObjectWithMemoryLimit_ThenOutOfMemoryIsThrown()
+		{
+			Process process = Process.Start(typeof(TestProgramMemoryUsage.Program).Assembly.Location);
+
+			using (var job = JobObjectHandle.Create())
+			{
+				job.SetInformation(new ExtendedLimitInformation
+				{
+					ProcessMemoryLimit = 100 * 1024 * 1024,
+				});
+				job.AssignProcess(process);
+			}
+
+			process.WaitForExit();
+
+			Assert.Equal(-1, process.ExitCode);
+		}
+	}
+}
---
+++ Tests/JobObjectTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JobObjectTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("JobObjectTests")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("d1219f09-a4c4-494a-b413-683e70c45f5a")]
