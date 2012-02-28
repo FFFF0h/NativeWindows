d8b5c6f8e63ceff427e77f19ef01c5c0cbddc69c
Christian Bay <cb@appharbor.com>
2012-02-28 16:44:17 +0100

    Add: Demo program (not working due to desktop security access is missing)

---
+++ Demo/ProcessStartDemoProgram/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.DirectoryServices.AccountManagement;
+using System.Security;
+using System.Security.Principal;
+using NativeWindows.ProcessAndThread;
+using NativeWindows.User;
+using ProcessStartInfo = NativeWindows.ProcessAndThread.ProcessStartInfo;
+
+namespace ProcessStartDemoProgram
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			const string username = "testuserfoo";
+			const string password = "1234";
+
+			GetOrCreateUser(username, password);
+
+			using (var userHandle = UserHandle.Logon(username, ".", GetSecureString(password)))
+			{
+				using (var environmentBlockHandle = EnvironmentBlockHandle.Create(userHandle, false))
+				{
+					var processStartInfo = new ProcessStartInfo();
+					string commandLine = string.Format("\"{0}\"", typeof(TestProgramWhileTrue.Program).Assembly.Location);
+					using (var processInformation = ProcessHandle.CreateAsUser(userHandle, null, commandLine, false, ProcessCreationFlags.CreateNewConsole | ProcessCreationFlags.CreateSuspended | ProcessCreationFlags.CreateUnicodeEnvironment, environmentBlockHandle, Environment.CurrentDirectory, processStartInfo))
+					{
+						Process process = Process.GetProcessById(processInformation.ProcessId);
+						processInformation.ThreadHandle.Resume();
+						Console.WriteLine("Press any key to kill");
+						Console.ReadKey(intercept: true);
+						process.Kill();
+
+						DeleteUser(username);
+					}
+				}
+			}
+		}
+
+		private static void GetOrCreateUser(string username, string password)
+		{
+			try
+			{
+				var account = new NTAccount(username);
+				var sid = account.Translate(typeof(SecurityIdentifier));
+			}
+			catch (IdentityNotMappedException)
+			{
+				using (var principalContext = new PrincipalContext(ContextType.Machine))
+				{
+					using (var userPrincipal = new UserPrincipal(principalContext))
+					{
+						userPrincipal.Name = username;
+						userPrincipal.SetPassword(password);
+						userPrincipal.Save();
+					}
+				}
+			}
+		}
+
+		private static void DeleteUser(string username)
+		{
+			using (var principalContext = new PrincipalContext(ContextType.Machine))
+			{
+				using (var userPrincipal = UserPrincipal.FindByIdentity(principalContext, username))
+				{
+					userPrincipal.Delete();
+				}
+			}
+		}
+
+		private static SecureString GetSecureString(string unsecurePassword)
+		{
+			var secureString = new SecureString();
+			foreach (char c in unsecurePassword)
+			{
+				secureString.AppendChar(c);
+			}
+			return secureString;
+		}
+	}
+}
---
+++ Demo/ProcessStartDemoProgram/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProcessStartDemoProgram")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("ProcessStartDemoProgram")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("cf121610-e028-4763-8f4f-e3d3b1bce9ae")]
