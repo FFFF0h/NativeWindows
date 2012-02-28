f3a01174a0f6816bfcc520fe243927e12f474720
Christian Bay <cb@appharbor.com>
2012-02-28 12:42:43 +0100

    Fix: ProcessInformation should be IDisposable so the contained handled is also disposed

--- NativeWindows/ProcessAndThread/ProcessInformation.cs
+++ NativeWindows/ProcessAndThread/ProcessInformation.cs
@@ -1,13 +1,20 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace NativeWindows.ProcessAndThread
 {
 	[StructLayout(LayoutKind.Sequential)]
-	public class ProcessInformation
+	public class ProcessInformation : IDisposable
 	{
 		public ProcessHandle ProcessHandle;
 		public ThreadHandle ThreadHandle;
 		public int ProcessId;
 		public int ThreadId;
+
+		public void Dispose()
+		{
+			ProcessHandle.Dispose();
+			ThreadHandle.Dispose();
+		}
 	}
 }
