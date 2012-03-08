42f3b17e9ae8df354ba77568bd55ade82cbd338b
Christian Bay <taz@nakke.dk>
2012-03-08 23:08:35 +0100

    Add enum: PipeAccessRights

---
+++ NativeWindows/IO/PipeAccessRights.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NativeWindows.IO
+{
+	[Flags]
+	public enum PipeAccessRights : uint
+	{
+		None = 0,
+
+		ReadData = 1,
+		WriteData = 2,
+		CreatePipeInstance = 4,
+		ReadAttributes = 128,
+		WriteAttributes = 256,
+
+		Delete = 0x00010000,
+		ReadPermissions = 0x00020000,
+		WritePermissions = 0x00040000,
+		TakeOwnership = 0x00080000,
+		Synchronize = 0x00100000,
+
+		Read = StandardAccessRights.Read | ReadData | CreatePipeInstance | ReadAttributes,
+		Write = StandardAccessRights.Write | WriteData | CreatePipeInstance,
+		Execute = StandardAccessRights.Execute,
+
+		AllAccess = StandardAccessRights.Required | ReadData | WriteData | CreatePipeInstance | ReadAttributes | WriteAttributes | Synchronize,
+	}
+}
