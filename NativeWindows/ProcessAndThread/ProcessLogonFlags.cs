6713ddedff6bae101bf1e190edf74851eb143ce8
Christian Bay <cb@appharbor.com>
2012-03-12 17:33:35 +0100

    Add enum: ProcessLogonFlags

---
+++ NativeWindows/ProcessAndThread/ProcessLogonFlags.cs
@@ -0,0 +1,10 @@
+﻿namespace NativeWindows.ProcessAndThread
+{
+	public enum ProcessLogonFlags : uint
+	{
+		None = 0,
+
+		LogonWithProfile = 0x00000001,
+		LogonNetcredentialsOnly = 0x00000002,
+	}
+}
