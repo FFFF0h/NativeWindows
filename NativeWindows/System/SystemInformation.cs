f9662539b624ebb275e8e1c72b37b8779997e2fa
Christian Bay <cb@appharbor.com>
2012-03-23 11:03:16 +0100

    Add class: SystemInformation

---
+++ NativeWindows/System/SystemInformation.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+using NativeWindows.ProcessAndThread;
+
+namespace NativeWindows.System
+{
+	public static class SystemInformation
+	{
+		private static class NativeMethods
+		{
+			[DllImport("kernel32.dll")]
+			public static extern void GetSystemInfo(out SystemInfo systemInfo);
+
+			[DllImport("kernel32.dll")]
+			public static extern void GetNativeSystemInfo(out SystemInfo systemInfo);
+		}
+
+		public static SystemInfo GetSystemInfo()
+		{
+			using (var processHandle = ProcessHandle.GetCurrentProcess())
+			{
+				SystemInfo systemInfo;
+				if (processHandle.IsWow64Process())
+				{
+					NativeMethods.GetNativeSystemInfo(out systemInfo);
+				}
+				else
+				{
+					NativeMethods.GetSystemInfo(out systemInfo);
+				}
+				return systemInfo;
+			}
+		}
+	}
+}
