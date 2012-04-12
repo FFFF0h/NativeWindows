68afbd517c48cdd0c03574a816b71485f9f4df20
Christian Bay <taz@nakke.dk>
2012-04-12 17:36:42 +0200

    Add small test program to check that GetTokenInformation works

---
+++ Demo/GetLogonSidDemoProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Security.Principal;
+using NativeWindows.User;
+
+namespace GetLogonSidDemoProgram
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var windowsIdentity = WindowsIdentity.GetCurrent())
+			{
+				using (var token = new UserHandle(windowsIdentity))
+				{
+					var groups = token.GetGroupsTokenInformation(TokenInformationClass.TokenLogonSid);
+					SecurityIdentifier securityIdentifier = groups.Single().SecurityIdentifier;
+					var isAccount = securityIdentifier.IsAccountSid();
+					var text = securityIdentifier.ToString();
+				}
+			}
+		}
+	}
+}
---
+++ Demo/GetLogonSidDemoProgram/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GetLogonSidDemoProgram")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("GetLogonSidDemoProgram")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8c266c7f-7ef6-48c3-8015-6686f738f5d8")]
