aaea22d15748ce5d40d567be7d9f6d36957f4362
Christian Bay <taz@nakke.dk>
2012-06-07 14:01:16 +0200

    Add class: LsaReferencedDomainsHandle

---
+++ NativeWindows/Identity/LsaReferencedDomainsHandle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NativeWindows.Identity
+{
+	public class LsaReferencedDomainsHandle : SafeHandle
+	{
+		private static class NativeMethods
+		{
+			[DllImport("advapi32")]
+			internal static extern LsaStatus LsaFreeMemory(IntPtr handle);
+		}
+
+		public LsaReferencedDomainsHandle(IntPtr handle, bool ownsHandle = true)
+			: base(handle, ownsHandle)
+		{
+		}
+
+		protected override bool ReleaseHandle()
+		{
+			return NativeMethods.LsaFreeMemory(handle) == LsaStatus.Success;
+		}
+
+		public override bool IsInvalid
+		{
+			get
+			{
+				return handle != IntPtr.Zero;
+			}
+		}
+	}
+}
