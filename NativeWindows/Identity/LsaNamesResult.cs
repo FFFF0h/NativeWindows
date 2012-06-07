e6b6e77cb33764b78fa37d9c6f5885a47766fe3c
Christian Bay <taz@nakke.dk>
2012-06-07 14:02:40 +0200

    Add class: LsaNamesResult
    This is a small wrapper LsaLookupNames2 return value

---
+++ NativeWindows/Identity/LsaNamesResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NativeWindows.Identity
+{
+	public class LsaNamesResult : IDisposable
+	{
+		public LsaNamesResult(LsaReferencedDomainsHandle referencedDomainsHandle, LsaTranslatedSidHandle translatedSidHandle)
+		{
+			ReferencedDomainsHandle = referencedDomainsHandle;
+			TranslatedSidHandle = translatedSidHandle;
+		}
+
+		public LsaReferencedDomainsHandle ReferencedDomainsHandle
+		{
+			get;
+			private set;
+		}
+
+		public LsaTranslatedSidHandle TranslatedSidHandle
+		{
+			get;
+			private set;
+		}
+
+		public void Dispose()
+		{
+			TranslatedSidHandle.Dispose();
+			ReferencedDomainsHandle.Dispose();
+		}
+	}
+}
