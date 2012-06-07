02085c2de42d328049318e74a58f1998ef89706e
Christian Bay <taz@nakke.dk>
2012-06-07 13:59:12 +0200

    Add: SidNameUse enum

---
+++ NativeWindows/Identity/SidNameUse.cs
@@ -0,0 +1,15 @@
+namespace NativeWindows.Identity
+{
+	public enum SidNameUse : int
+	{
+		User = 1,
+		Group = 2,
+		Domain = 3,
+		Alias = 4,
+		KnownGroup = 5,
+		DeletedAccount = 6,
+		Invalid = 7,
+		Unknown = 8,
+		Computer = 9,
+	}
+}
