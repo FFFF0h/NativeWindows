f3a2c6a9f59d6c28fa8479c56141a9740fd7bdbe
Christian Bay <cb@appharbor.com>
2012-06-08 10:52:51 +0200

    Add class: DisposableExtensions

---
+++ NativeWindows/DisposableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NativeWindows
+{
+	internal static class DisposableExtensions
+	{
+		public static void DisposeAll<T>(this IEnumerable<T> disposables)
+			where T : IDisposable
+		{
+			foreach (var disposable in disposables)
+			{
+				disposable.Dispose();
+			}
+		}
+	}
+}
