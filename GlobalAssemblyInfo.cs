8614408c86bf3b256ad331800f1d9234d8529b09
Christian Bay <cb@appharbor.com>
2012-07-18 16:59:15 +0200

    Increased version 1.3.3

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.1.2.3")]
-[assembly: AssemblyFileVersion("0.1.2.3")]
+[assembly: AssemblyVersion("0.1.3.3")]
+[assembly: AssemblyFileVersion("0.1.3.3")]
