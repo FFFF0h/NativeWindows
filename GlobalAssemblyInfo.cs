c53103e12af3525417d6962b1ae8d7be6b7b6712
Christian Bay <cb@appharbor.com>
2012-08-28 12:17:21 +0200

    Update to version 1.5.3

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.1.5.2")]
-[assembly: AssemblyFileVersion("0.1.5.2")]
+[assembly: AssemblyVersion("0.1.5.3")]
+[assembly: AssemblyFileVersion("0.1.5.3")]
